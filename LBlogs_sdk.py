48f5bc4fee43e17fbdfeb0ba44fe7e12876d1c0e
Lance Liang <LanceLiang2018@163.com>
2019-03-17 16:23:28 +0800

    debug

--- LBlogs_sdk.py
+++ LBlogs_sdk.py
@@ -1,8 +1,10 @@
 import requests
+import io
 
 
 class LBlogsClient:
     def __init__(self):
+        # self.API = "http://lanceliang2018.xyz:5000/v1/api"
         self.API = "http://127.0.0.1:5000/v1/api"
         self.LOGIN = 'login'
         self.SIGNUP = 'signup'
@@ -10,18 +12,27 @@ class LBlogsClient:
         self.GET_FILES = 'get_files'
         self.GET_USER = 'get_user'
         self.SET_USER = 'set_user'
+        self.PUBLISH = 'publish'
 
         self.username = ''
         self.auth = ''
 
     # With Auth
-    def post(self, action: str, params: dict):
+    def post(self, action: str, params: dict, files=None):
         params['action'] = action
         params['auth'] = self.auth
-        r = requests.post(self.API, data=params)
+        r = requests.post(self.API, data=params, files=files)
         if r.status_code != 200:
-            return {'code': '-1', 'message': "Server Error."}
+            return {'code': -1, 'message': "Server Error."}
         return r.json()
+
+    def post_content(self, action: str, params: dict, files=None):
+        params['action'] = action
+        params['auth'] = self.auth
+        r = requests.post(self.API, data=params, files=files)
+        if r.status_code != 200:
+            return {'code': -1, 'message': "Server Error."}
+        return r.content
 
     def login(self, username, password):
         result = self.post(self.LOGIN, {'username': username, 'password': password})
@@ -38,9 +49,23 @@ class LBlogsClient:
             return False
         return True
 
+    def publish(self, zipdata: bytes):
+        files = {'zipfile': io.BytesIO(zipdata)}
+        result = self.post(self.PUBLISH, {}, files=files)
+        if not result['code'] == 0:
+            print(result)
+            return False
+        return True
+
 
 if __name__ == '__main__':
     client = LBlogsClient()
-    if client.signup('Tony', ''):
-        client.login('Tony', '')
+    if not client.login('Tony', ''):
+        if not client.signup('Tony', ''):
+            exit(1)
+
+    with open('test.zip', 'rb') as f:
+        zipdata = f.read()
+
+    print(client.publish(zipdata))
 
